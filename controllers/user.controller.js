3d76046077b19acefe972bec88e247a353c38104
cormacx <gaio.lucas0@gmail.com>
2022-05-27 23:13:11 -0300

    feat: returns not found if result is false on findByPk

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -21,7 +21,13 @@ function read(req, res) {
     const id = req.params.id;
 
     models.User.findByPk(id).then(result => {
-        res.status(200).json(result);
+        if (result) {
+            res.status(200).json(result);
+        } else {
+            res.status(404).json({
+                message: "User not found"
+            });
+        }
     }).catch(error => {
         res.status(500).json({
             message: "Something went wrong!"
