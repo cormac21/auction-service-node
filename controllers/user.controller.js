40b3022585ab4acceecf260e2bc2ce054590ccca
cormacx <gaio.lucas0@gmail.com>
2022-06-01 22:17:17 -0300

    feat: more user stuff;

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,33 +1,60 @@
-const Validator = require('fastest-validator')
-const models = require('../models')
+const Validator = require('fastest-validator');
+const models = require('../models');
+const bcryptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 function save(req, res) {
-    const user = {
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    }
 
-    const schema = {
-        email: {type: "string", optional: false},
-        password: {type: "string", optional: false}
-    }
+    models.User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then( result => {
+        if (result) {
+            res.status(409).json({
+                message: "Email already exists!"
+            })
+        } else {
+            bcryptjs.genSalt(10, function(err, salt){
+                bcryptjs.hash(req.body.password, salt, function(err, hash){
+                    const user = {
+                        name: req.body.name,
+                        email: req.body.email,
+                        password: hash
+                    }
 
-    const v = new Validator();
-    const validationObj = v.validate(user, schema);
+                    const schema = {
+                        email: {type: "string", optional: false},
+                        password: {type: "string", optional: false}
+                    }
 
-    if ( validationObj !== true ) {
-        return res.status(400).json({
-           message: "Validation failed",
-           errors: validationObj
-        });
-    }
+                    const v = new Validator();
+                    const validationObj = v.validate(user, schema);
 
-    models.User.create(user).then( result => {
-        res.status(201).json(result);
-    }).catch(error => {
+                    if ( validationObj !== true ) {
+                        return res.status(400).json({
+                            message: "Validation failed",
+                            errors: validationObj
+                        });
+                    }
+
+                    models.User.create(user).then( result => {
+                        res.status(201).json({
+                            message: "User created successfully"
+                        });
+                    }).catch(error => {
+                        res.status(500).json({
+                            message: "Something went wrong",
+                            error: error
+                        })
+                    });
+                });
+            });
+        }
+    }).catch( error => {
         res.status(500).json({
-            message: "Something went wrong"
+            message: "Something went wrong",
+            error: error
         })
     });
 }
@@ -95,7 +122,42 @@ function destroy(req, res) {
             message: "Something went wrong!"
         })
     });
+}
 
+function login(req, res) {
+    models.User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(user => {
+        if ( user === null) {
+            res.status(401).json({
+                message: "Invalid Credentials!"
+            })
+        } else {
+            bcryptjs.compare(req.body.password, user.password, function(error, result){
+                if (result) {
+                    const token = jwt.sign({
+                        email: user.email,
+                        roles: user.roles
+                    }, 'secret', function(error, token){
+                        res.status(200).json({
+                           message: "Authentication successful!",
+                           token: token
+                        });
+                    });
+                } else {
+                    res.status(401).json({
+                        message: "Invalid Credentials!"
+                    })
+                }
+            });
+        }
+    }).catch(error => {
+        res.status(500).json({
+            message: "Something went wrong!"
+        })
+    });
 }
 
 module.exports = {
@@ -103,5 +165,6 @@ module.exports = {
     read: read,
     index: index,
     update: update,
-    destroy: destroy
+    destroy: destroy,
+    login: login
 }
--- routes/users.js
+++ routes/users.js
@@ -8,5 +8,6 @@ router.post('/', usersController.save);
 router.get('/:id', usersController.read);
 router.put('/:id', usersController.update);
 router.delete('/:id', usersController.destroy);
+router.post('/login', usersController.login);
 
 module.exports = router;
